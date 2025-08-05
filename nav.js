38a6101397f642a1e6d6086e0300611294c4ee22
Petr Fifka <petr.fifka@tredgate.cz>
2025-08-05 17:38:23 +0200

    change index.html and nav.js to meet requirements from playwright/cypress academy

--- nav.js
+++ nav.js
@@ -3,12 +3,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navMenu = `
     <ul>
-      <li><a href="index.html">Home</a></li>
-      <li><a href="dynamic-content.html">Dynamic Content</a></li>
-      <li><a href="contact.html">Contact</a></li>
-      <li><a href="registration.html">Registration</a></li>
-      <li><a href="web-actions.html">Web Actions</a></li>
-      <li><a href="aria-testing.html">ARIA Testing</a></li>
+      <li><a href="index.html" id="home">Home</a></li>
+      <li><a href="dynamic-content.html" id="dynamic-content">Dynamic Content</a></li>
+      <li><a href="contact.html" id="contact">Contact</a></li>
+      <li><a href="registration.html" id="registration">Registration</a></li>
+      <li><a href="web-actions.html" id="web-actions">Web Actions</a></li>
+      <li><a href="aria-testing.html" id="aria-testing">ARIA Testing</a></li>
     </ul>
   `;
   const nav = document.getElementById("main-nav");
